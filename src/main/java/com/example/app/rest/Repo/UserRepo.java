e051e3a52298acf354311acd9d19b1a9d456c871
jonas <jonasweert@hotmail.com>
2023-04-01 15:39:40 +0200

    First Commit

---
+++ src/main/java/com/example/app/rest/Repo/UserRepo.java
@@ -0,0 +1,9 @@
+package com.example.app.rest.Repo;
+
+import com.example.app.rest.Models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepo extends JpaRepository<User, Long> {
+
+
+}
